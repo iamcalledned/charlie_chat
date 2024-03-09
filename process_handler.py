c828c8d5883d26ee09967648b362c1648edd5a2c
Ned <iamcalledned@gmail.com>
2024-03-09 12:56:47 -0500

    Update process_handler.py

--- process_handler.py
+++ process_handler.py
@@ -275,4 +275,4 @@ async def get_session(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
